6dfb09dde7f3561acfd1eca03aac6d3b952263d8
kyohei-tsuno <k.tsuno@ichain.co.jp>
2020-06-21 18:47:41 +0900

    add ThrottleRequest

---
+++ app/Http/Middleware/RateLimitting/ThrottlesRequest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Middleware\RateLimitting;
+
+use Closure;
+use Illuminate\Cache\RateLimiter;
+use Symfony\Component\HttpFoundation\Response;
+
+// MEMO：Illuminate\Routing\Middleware\ThrottleRequests.phpを元に変更
+
+class RateLimitThrottleRequests
+{
+
+    /**
+     * The rate limiter instance.
+     *
+     * @var \Illuminate\Cache\RateLimiter
+     */
+    protected $limiter;
+
+    /**
+     * Create a new request throttler.
+     *
+     * @param  \Illuminate\Cache\RateLimiter $limiter
+     */
+    public function __construct(RateLimiter $limiter)
+    {
+        $this->limiter = $limiter;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  int|string  $maxAttempts
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle($request, Closure $next)
+    {
+        $key = $this->resolveRequestSignature($request);
+
+        $maxAttempts = (int) config('app.maxAttempts');
+
+        if ($this->limiter->tooManyAttempts($key, $maxAttempts)) {
+            return $this->buildResponse($key, $maxAttempts);
+        }
+
+        $this->limiter->hit($key, 60);
+
+        $response = $next($request);
+
+        return $this->addHeaders(
+            $response, $maxAttempts,
+            $this->calculateRemainingAttempts($key, $maxAttempts)
+        );
+    }
+
+    /**
+     * Resolve request signature.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected function resolveRequestSignature($request)
+    {
+
+        if ($route = $request->route()) {
+            return sha1($route->getDomain().'|'.$request->ip());
+        }
+
+        throw new RuntimeException('Unable to generate the request signature. Route unavailable.');
+    }
+
+    /**
+     * Create a 'too many attempts' response.
+     *
+     * @param  string $key
+     * @param  int $maxAttempts
+     * @return \Illuminate\Http\Response
+     */
+    protected function buildResponse($key, $maxAttempts)
+    {
+        $errorinfo = json_encode([
+            'status_code' => '429',
+            'error' => 'Too Many Requests',
+            'error_description' => 'アクセス数が上限に達しました。',
+        ]);
+
+        $response = new Response($errorinfo, 429);
+
+        $retryAfter = $this->limiter->availableIn($key);
+
+        return $this->addHeaders(
+            $response, $maxAttempts,
+            $this->calculateRemainingAttempts($key, $maxAttempts, $retryAfter),
+            $retryAfter
+        );
+    }
+
+    /**
+     * Add the limit header information to the given response.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Response $response
+     * @param  int $maxAttempts
+     * @param  int $remainingAttempts
+     * @param  int|null $retryAfter
+     * @return \Illuminate\Http\Response
+     */
+    protected function addHeaders(Response $response, $maxAttempts, $remainingAttempts, $retryAfter = null)
+    {
+        $headers = [
+            'X-RateLimit-Limit' => $maxAttempts,
+            'X-RateLimit-Remaining' => $remainingAttempts,
+        ];
+
+        if (!is_null($retryAfter)) {
+            $headers['Retry-After'] = $retryAfter;
+            $headers['Content-Type'] = 'application/json';
+        }
+
+        $response->headers->add($headers);
+
+        return $response;
+    }
+
+    /**
+     * Calculate the number of remaining attempts.
+     *
+     * @param  string $key
+     * @param  int $maxAttempts
+     * @param  int|null $retryAfter
+     * @return int
+     */
+    protected function calculateRemainingAttempts($key, $maxAttempts, $retryAfter = null)
+    {
+        if (!is_null($retryAfter)) {
+            return 0;
+        }
+
+        return $this->limiter->retriesLeft($key, $maxAttempts);
+    }
+
+}
